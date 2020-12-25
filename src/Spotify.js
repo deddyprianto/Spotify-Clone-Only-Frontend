cc3be4b4bb9429c7b8125bda5956b32d2fddf40d
dedy sihombing <deddyprianto98@gmail.com>
2020-12-25 11:31:51 +0700

    merubah file spotify.js

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,5 +1,5 @@
 export const authEndPoint = "https://accounts.spotify.com/authorize";
-const redirectURL = "http://localhost:3000/";
+const redirectURL = "https://myy-spotify.herokuapp.com/";
 const clientID = "4a2e36561e3b44abace223f55e7e282b";
 const token =
   "BQBPv87oW3KMlY_nHmweNAzo56PhlzhO9YU-CKUo64CrUyRdbsgMZGQiukbQfyovI3DKYHqgZ_4OD41Zi6_3QFNuOKjoQ5TdT12fX5-nZR8tBpHamK3yXglm6cELUwCzeclmXgwZfME_uEWqD0T9ZgNa5JdetlsBb-ncBuj_c4jPItNp8-s";
